de0496d98c5395c848fb1cd36758210faa629c9a
Nikoloz Ebralidze <NEbralidze@seu.edu.ge>
2025-09-18 02:26:30 +0400

    add configutarion for github pagee v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [vue()],
   base: '/Portfolio/', // This should match your GitHub repository name
   resolve: {
