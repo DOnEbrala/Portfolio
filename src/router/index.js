94daab90c11cfd4b309d0fc8bf9d6b755562a53d
Nikoloz Ebralidze <NEbralidze@seu.edu.ge>
2025-10-04 14:45:07 +0400

    add changes

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/Portfolio/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
